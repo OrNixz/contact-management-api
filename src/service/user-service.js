8d1c551c489695bd072b74a203bf46547f8f6927
OrNixz <ramadhanafif172@gmail.com>
2024-04-21 14:06:05 +0700

    feat: Implement user registration with validation and error handling

---
+++ src/service/user-service.js
@@ -0,0 +1,31 @@
+import { prismaClient } from "../app/database.js";
+import { ResponseError } from "../error/response-error.js";
+import { registerUserValidation } from "../validation/user-validation.js";
+import { validate } from "../validation/validation.js";
+import bcrypt from "bcrypt";
+
+const register = async (request) => {
+  const user = validate(registerUserValidation, request);
+
+  const countUser = await prismaClient.user.count({
+    where: {
+      username: user.username,
+    },
+  });
+
+  if (countUser === 1) {
+    throw new ResponseError(400, "username is already taken");
+  }
+
+  user.password = await bcrypt.hash(user.password, 10);
+
+  return prismaClient.user.create({
+    data: user,
+    select: {
+      username: true,
+      name: true,
+    },
+  });
+};
+
+export default { register };
